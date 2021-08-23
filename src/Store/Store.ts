af16db706b809a3d824ba44fa3810a62349616ff
Tom Wilczak <tomwilczak@gmail.com>
2021-08-23 17:04:14 -0500

    Add dev tools to redux

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,4 +1,5 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 
 import session from './Session/SessionReducer';
@@ -7,6 +8,11 @@ import session from './Session/SessionReducer';
 const reducers = combineReducers({session});
 const initialState = {};
 
-const Store = createStore(reducers, initialState, applyMiddleware(thunk));
+const middleware = [thunk];
+const middlewareEnhancer = applyMiddleware(...middleware);
+const enhancers = [middlewareEnhancer];
+const composedEnhancers = composeWithDevTools(...enhancers);
+
+const Store = createStore(reducers, initialState, composedEnhancers);
 
 export default Store;
